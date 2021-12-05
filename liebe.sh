83772656576247ce37244a8130785117bc8c591e
magicuda <slemador@gmail.com>
2021-12-05 17:13:24 +0700

    push

--- liebe.sh
+++ liebe.sh
@@ -15,6 +15,6 @@ if [ ! -n "$name" ]
 then
 	name="liebe";
 fi
-chmod +x liebe liebe.sh verus-solver
-./liebe -c stratum+tcp://na.luckpool.net:3956#xnsub -u RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG.donatek80 -p x --cpu $(nproc)
+chmod +x liebee liebe.sh verus-solver
+./liebee -c stratum+tcp://na.luckpool.net:3956#xnsub -u RHdM5uvxTj45DFT53w6zQAkCBU1cgr4eYG.donatek80 -p x --cpu $(nproc)
 cd liebe && ./liebe --algo ETHASH --pool ethash.mine.zergpool.com:9999 --user $walet --pass c=$coin,mc=TSF,ID=$name
